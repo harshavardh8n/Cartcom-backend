05ba8819ea93db2069d1899e09e957e2ea5061a6
harshavardh8n <harshavardhangaikwad39@gmail.com>
2024-08-26 12:32:16 +0530

    for testing

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const cors = require("cors");
-// const router = require("./router/auth-router")
-// const crouter = require("./router/contact-router")
-// const dbconnect =require("./utils/db");
-// const prouter = require("./router/product-router");
+const router = require("./router/auth-router")
+const crouter = require("./router/contact-router")
+const dbconnect =require("./utils/db");
+const prouter = require("./router/product-router");
 const app = express();
 
 // lets tackle cors
@@ -19,18 +19,18 @@ app.get("/product", (req,res)=>{
 })
 
 
-// app.use(router);
-// app.use(crouter);
-// app.use(prouter);
-// router.route()
+app.use(router);
+app.use(crouter);
+app.use(prouter);
+router.route()
 
 app.get("/", (req,res)=>{
     res.send("Working");
 })
 
 const PORT =process.env.PORT || 4000;
-// dbconnect().then(()=>{
+dbconnect().then(()=>{
     app.listen(PORT,()=>{
         console.log("server running on",PORT);
     })
-// })
+})
